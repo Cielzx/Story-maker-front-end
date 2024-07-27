285ed942bdba03cca6dccbf0b638768612fbdc19
Cielzx <santosjaciel30@gmail.com>
2024-07-26 23:57:04 -0300

    Feature: Temporary landind page

--- src/app/components/Loading/index.tsx
+++ src/app/components/Loading/index.tsx
@@ -1,9 +1,14 @@
+"use client";
 import { Spinner } from "@chakra-ui/spinner";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FaArrowRight } from "react-icons/fa";
 
 const Loading = () => {
+  const pathname = usePathname();
   return (
-    <main className="min-h-screen relative flex items-center justify-center backgroundDash text-white">
-      <div className="flex relative">
+    <main className="min-h-screen relative flex flex-col gap-4 items-center justify-center z-[99999px] text-white">
+      <div className="flex relative ">
         <img
           src="https://images2.imgbox.com/68/26/AGtl8eED_o.png"
           className="w-full object-contain absolute  h-[100%]"
@@ -11,6 +16,21 @@ const Loading = () => {
         />
         <Spinner className="w-[230px] h-[230px]" color="white" />
       </div>
+
+      {pathname === "/" ? (
+        <>
+          <div className="flex flex-col w-[100%] gap-[10px] items-center justify-center p-2">
+            <Link
+              className="w-[60%] h-[40px] flex justify-center items-center gap-2 rounded-sm bg-purple-600 text-[white] text-center text-2xl"
+              href={"/login"}
+            >
+              Entre agora <FaArrowRight />
+            </Link>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </main>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
+import Loading from "./components/Loading";
 
 export default function Home() {
   return (
-    <main className="flex w-[100%] min-h-screen flex-col items-center backgroundImage justify-between ">
+    <main className="flex w-[100%] min-h-screen flex-col items-center justify-between">
+      <div className="backgroundImage"></div>
       <section className="flex flex-col justify-between w-[100%] h-[100vh] text-white">
-        <div className="flex w-[100%]  flex-col justify-between gap-[40px]  border border-solid border-red-600 p-4 ">
+        {/* <div className="flex w-[100%]  flex-col justify-between gap-[40px]  border border-solid border-red-600 p-4 ">
           <h1>Story Maker</h1>
 
           <p className="text-3xl">
             Sejam bem vindos a story makes, onde você pode compartilhar
             figurinhas no seu stories ^^
           </p>
-        </div>
+        </div> */}
+
+        <Loading />
 
         <div className="flex flex-col w-[100%] gap-[10px] items-center justify-center p-2">
           <Link
