6b48e7f6827271ab00e2069103862ba45ec7ee12
Cielzx <santosjaciel30@gmail.com>
2024-07-28 03:25:29 -0300

    Fix: Erro due the next router montage solved

--- src/app/dashboard/[id]/layout.tsx
+++ src/app/dashboard/[id]/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { parseCookies } from "nookies";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -12,7 +12,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   }
 
   return (
-    <div className="w-full flex flex-col min-h-screen">
+    <div className="w-full flex flex-col h-svh">
       <div className="backgroundDash"></div>
       <main className="w-full flex flex-col flex-grow bg-cover bg-center text-white items-center ">
         <section className="w-full flex flex-col h-[100vh]">{children}</section>
