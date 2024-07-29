a27ea6bd5f4254a2fbb3eae2f1d72d9725a01cae
Cielzx <santosjaciel30@gmail.com>
2024-07-29 20:37:45 -0300

    Css changes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,14 +5,7 @@ import { useRouter } from "next/navigation";
 const RootDashLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative h-svh">
-      <div
-        className="h-full absolute left-0 top-0 right-0 bottom-0 flex z-0"
-        style={{
-          visibility: "inherit",
-        }}
-      >
-        {children}
-      </div>
+      <div className="h-full">{children}</div>
     </div>
   );
 };
