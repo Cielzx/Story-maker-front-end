275aef9ec9e00b57ddde719828d9a10ebf11c651
Cielzx <santosjaciel30@gmail.com>
2024-07-30 11:54:44 -0300

    Fix: Css breaking the category page

--- src/app/dashboard/[id]/layout.tsx
+++ src/app/dashboard/[id]/layout.tsx
@@ -5,7 +5,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="w-full flex flex-col max-md:h-[92vh]">
       <div className="backgroundDash"></div>
       <main className="w-full flex flex-col flex-grow bg-cover bg-center text-white items-center ">
-        <section className="w-full flex flex-col h-[100vh]">{children}</section>
+        <section className="w-full flex flex-col">{children}</section>
       </main>
     </div>
   );
