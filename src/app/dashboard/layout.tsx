61a3f3683c96e159324ff8004d35672255905cf9
Cielzx <santosjaciel30@gmail.com>
2024-08-21 19:10:34 -0300

    Update: CSS CHANGE

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ const RootDashLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <>
-      <main className="flex-[1] w-full h-full flex-grow ">
+      <main className="w-full h-full flex-grow ">
         {children}
 
         <DashMenu />
