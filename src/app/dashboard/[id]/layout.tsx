c513e6a0fb3a83047667d37a9709e2a10a440ae1
Cielzx <santosjaciel30@gmail.com>
2024-08-22 18:52:05 -0300

    Fix: isLoading active again

--- src/app/dashboard/[id]/layout.tsx
+++ src/app/dashboard/[id]/layout.tsx
@@ -3,7 +3,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="w-full h-full flex flex-grow flex-col ">
       <div className="backgroundDash"></div>
       <div className="w-full h-full flex flex-col bg-cover bg-center text-white items-center ">
-        <section className="w-full h-full  flex flex-col">{children}</section>
+        <section className="w-full h-full flex flex-col">{children}</section>
       </div>
     </div>
   );
