29dfaa38d7feb43b952348818a56702caa5d6f9b
Cielzx <santosjaciel30@gmail.com>
2024-08-22 18:30:28 -0300

    Test: Device verifier  in iphone

--- src/app/components/Loading/index.tsx
+++ src/app/components/Loading/index.tsx
@@ -5,21 +5,33 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { parseCookies } from "nookies";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 
 const Loading = () => {
   const pathname = usePathname();
   const router = useRouter();
+  const [height, setHeight] = useState("");
   const { getUser, user } = useUSer();
   const cookies = parseCookies();
 
   useEffect(() => {
+    const userAgent = navigator.userAgent;
+    if (/iPad|iPhone|iPod/.test(userAgent)) {
+      setHeight("-webkit-fill-available");
+    } else {
+      setHeight("100%");
+    }
     getUser();
   }, [user]);
 
   return (
-    <div className="w-full heightFill relative flex flex-col flex-grow gap-4 items-center justify-center z-[99999px] text-white">
+    <div
+      className="w-full relative flex flex-col flex-grow gap-4 items-center justify-center z-[99999px] text-white"
+      style={{
+        height: height,
+      }}
+    >
       <div className="backgroundDash"></div>
       <div className="w-full h-full flex relative flex-grow justify-center items-center">
         <img
