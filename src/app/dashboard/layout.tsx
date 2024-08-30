981e7e9314ae00e66fbe5920b1191621fa9460d4
Cielzx <santosjaciel30@gmail.com>
2024-08-30 11:58:47 -0300

    Update: Serch css in the category page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,41 +3,17 @@ import DashMenu from "./components/dashMenu";
 import NotRenewed from "../components/NotRenewedModal";
 import { useDisclosure } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { useUSer } from "@/hooks";
-import { differenceInDays, isAfter } from "date-fns";
 
 const RootDashLayout = ({ children }: { children: React.ReactNode }) => {
   const { onClose, isOpen, onOpen } = useDisclosure();
-  const { user, getUser, setMode } = useUSer();
 
-  const paymentCheckout = () => {
-    const now = new Date();
-    if (user && user.subscription) {
-      const nextPayment = new Date(user.subscription.next_payment);
+  return (
+    <main className="w-full h-full flex-grow ">
+      {children}
 
-      if (!isAfter(now, nextPayment)) {
-        onClose();
-      }
-
-      if (isAfter(now, nextPayment)) {
-        const daysLate = differenceInDays(now, nextPayment);
-
-        if (daysLate > 0) {
-          onOpen();
-
-          return (
-            <NotRenewed onClose={onClose} onOpen={onOpen} isOpen={isOpen} />
-          );
-        }
-      }
-    }
-  };
-
-  useEffect(() => {
-    paymentCheckout();
-  }, [user]);
-
-  return <main className="w-full h-full flex-grow ">{children}</main>;
+      <NotRenewed onClose={onClose} isOpen={isOpen} onOpen={onOpen} />
+    </main>
+  );
 };
 
 export default RootDashLayout;
